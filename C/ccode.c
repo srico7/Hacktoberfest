278b611a18c4e389ebed5ec568c7cb8b9c7de09b
jayesh <jjdkarchchi@students.nsbm.ac.lk>
2022-10-29 11:51:42 +0530

    codec

--- C/ccode.c
+++ C/ccode.c
@@ -1,14 +1,1 @@
-#include <stdio.h>
-int main() {
-
-    int num1, num2, sum;
-
-    printf("Enter two integers: ");
-    scanf("%d %d", &num1, &num2);
-
-    // calculating sum
-    sum = num1 + num2;
-
-    printf("%d + %d = %d", num1, num2, sum);
-    return 0;
-}
+c
